07e92cb992d190affa849c89dfc4f97d9d6c6cfd
Drew Wells <drew.wells00@gmail.com>
2014-01-03 11:09:48 -0600

    rename to main.go

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/joho/godotenv"
+	//"encoding/json"
+	//"html/template"
+	"errors"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+var validPath = regexp.MustCompile("^/(view)/(.+)$")
+var PID string
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		//log.Fatal("Error loading .env file")
+	}
+	PID = os.Getenv("PID")
+	http.HandleFunc("/view/", makeHandler(ViewHandler))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+	return m[2], nil // The title is the second subexpression.
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		//Here we will extract the page title from the Request,
+		// and call the provided handler 'fn'
+		m := validPath.FindStringSubmatch(r.URL.Path)
+
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
